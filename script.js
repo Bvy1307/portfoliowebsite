0691d36893ba5ec2af5d8f8ad1e60dc205601c89
bhavya13 <117834105+Bvy1307@users.noreply.github.com>
2026-02-25 21:13:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Developer"],
+    strings : ["Network Engineer","Designer","Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
